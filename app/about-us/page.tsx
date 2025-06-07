55e799ba82b26568a8c1a551c3b7899dff5af3be
Weiß <tory020900@gmail.com>
2025-06-07 18:33:13 +0900

    make page and url

---
+++ app/about-us/page.tsx
@@ -0,0 +1,9 @@
+export default function AobiyUs() {
+    return (
+        <div>
+        <h1>About Us</h1>
+        <p>We are a company that values innovation and excellence.</p>
+        <p>Our mission is to provide the best services to our customers.</p>
+        </div>
+    );
+    }
---
+++ app/about-us/sales/page.tsx
@@ -0,0 +1,9 @@
+export default function sales() {
+    return (
+        <div>
+            <h1>Sales Team</h1>
+            <p>Welcome to our sales page!</p>
+            <p>Here you can find the best deals and offers.</p>
+        </div>
+    );
+}
